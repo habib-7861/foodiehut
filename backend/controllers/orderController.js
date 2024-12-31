a3f469994d0c5d6b98171acfb959f5f2599ce945
habib-7861 <120159688+habib-7861@users.noreply.github.com>
2024-12-31 15:21:06 +0500

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_kEY)
 //placing user order from frontend
 const placeOrder = async (req,res)=>{
 
-    const frontend_url = "http://localhost:5174";
+    const frontend_url = "https://silver-sfogliatella-06c01f.netlify.app/";
 
     try {
         const newOrder = new orderModel({
